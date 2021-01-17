309f657a994ff9cf736c25b73ec8336600338128
david-fernando <davidfernandodamata21@gmail.com>
2021-01-17 18:03:59 -0300

    :ok_hand: refactor

--- pages/api/api.js
+++ pages/api/api.js
@@ -9,7 +9,6 @@ function requestApi(){
   }
   async function getGit(){
     const data = await request('https://api.github.com/users/david-fernando/repos')
-    console.log(data)
     return [
       data[4],
       data[3],
