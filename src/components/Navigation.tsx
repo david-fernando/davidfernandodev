b1b759bada90ccf65489a6ac81207ddaa79bf48f
david-fernando <davidfernandodamata21@gmail.com>
2023-09-14 23:53:23 -0300

    Set width and heght on image

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import style from '@/styles/components/Navigation.module.css'
 import logo from '@/images/logo.svg'
-import Link from 'next/link'
 
 function Natigation(){
   return (
     <nav className={style.navgationContainer}>
       <div className={style.content}>
         <span className={style.logotipo}>
-          <Image src={logo} alt='logo' />
+          <Image src={logo} alt='logo' width={34} height={34} />
           <p>David Fernando</p>
         </span>
         <span className={style.links}>
