065af0c9fe22b8f081a524c364d56d65aa7f7ebe
david-fernando <davidfernandodamata21@gmail.com>
2023-09-17 23:35:12 -0300

    Add Projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Natigation from "@/components/Navigation"
 import Banner from "@/components/Banner"
 import Services from "@/components/Services"
 import WhatsAppButton from "../components/WhatsAppButton"
+import Projects from "@/components/Projects"
 // import { Inter } from 'next/font/google'
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -13,6 +14,7 @@ export default function Home() {
       <Banner />
       <Services />
       <WhatsAppButton />
+      <Projects />
     </div>
   )
 }
