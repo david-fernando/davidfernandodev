659e32cce0af86e11e87fc1a8eaef87b1ba8eb8c
david-fernando <davidfernandodamata21@gmail.com>
2023-09-17 22:27:04 -0300

    Add linkActived condition

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import style from '@/styles/components/Navigation.module.css'
 import logo from '@/images/logo.svg'
 
 function Natigation(){
+  const [ LinkActived, setLinkActived ] = useState('home')
+
   return (
     <nav className={style.navgationContainer}>
       <div className={style.content}>
@@ -12,10 +15,34 @@ function Natigation(){
           <p>David Fernando</p>
         </span>
         <span className={style.links}>
-          <Link className={style.active} href="/">Início</Link>
-          <Link href="#services">Serviços</Link>
-          <Link href="/">Projetos</Link>
-          <Link href="/">Artigos</Link>
+          <Link
+           onClick={() => setLinkActived('home')} 
+           className={(LinkActived === 'home')? style.active: ''} 
+           href="/"
+           >
+            Início
+          </Link>
+          <Link
+           onClick={() => setLinkActived('services')} 
+           className={(LinkActived === 'services')? style.active: ''} 
+           href="#services"
+           >
+            Serviços
+          </Link>
+          <Link
+           onClick={() => setLinkActived('projects')} 
+           className={(LinkActived === 'projects')? style.active: ''} 
+           href="/"
+           >
+            Projetos
+          </Link>
+          <Link
+           onClick={() => setLinkActived('articles')} 
+           className={(LinkActived === 'articles')? style.active: ''} 
+           href="/"
+           >
+            Artigos
+          </Link>
         </span>
       </div>
     </nav>
