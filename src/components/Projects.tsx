d9711bb80c6f2825e24913bb07b14a8954e00061
david-fernando <davidfernandodamata21@gmail.com>
2023-09-18 01:08:29 -0300

    Add mediumpostcard and mediumpostsapi link

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,6 +3,10 @@ import Link from 'next/link'
 import { IconExternalLink } from '@tabler/icons-react'
 
 import binaryTraders from '@/images/binarytraders.png'
+import obsignals from '@/images/obsignals.png'
+import jdoispisos from '@/images/jdoispisos.png'
+import mediumpostscard from '@/images/mediumpostscard.png'
+import mediumpostsapi from '@/images/mediumpostsapi.png'
 
 import style from '@/styles/components/Projects.module.css'
 
@@ -19,6 +23,43 @@ function Projects(){
             <Link href='https://binarytraders.net/' target='_blank' >Acessar <IconExternalLink /></Link>
           </div>
         </span>
+
+        <span className={style.project}>
+          <Image src={obsignals} alt='Obsignals' />
+          <div>
+            <p>Obsignals</p>
+            <p>Lorem ipsum dolor sit amet consectetur. Odio tortor pulvinar arcu duis in quam amet rhoncus. Sit sed gravida elit libero lectus suspendisse sit non ipsum. Tincidunt tortor molestie cursus gravida. Praesent in lectus quam ultricies magna erat.</p>
+            <Link href='https://chrome.google.com/webstore/detail/obsignals/amkbjfeoamfehgknhbnpggkkcmelkebb' target='_blank' >Acessar <IconExternalLink /></Link>
+          </div>
+        </span>
+
+        <span className={style.project}>
+          <Image src={jdoispisos} alt='J2PISOS' />
+          <div>
+            <p>J2PISOS</p>
+            <p>Lorem ipsum dolor sit amet consectetur. Odio tortor pulvinar arcu duis in quam amet rhoncus. Sit sed gravida elit libero lectus suspendisse sit non ipsum. Tincidunt tortor molestie cursus gravida. Praesent in lectus quam ultricies magna erat.</p>
+            <Link href='http://jdoispisos.com.br/' target='_blank' >Acessar <IconExternalLink /></Link>
+          </div>
+        </span>
+
+        <span className={style.project}>
+          <Image src={mediumpostscard} alt='Medium Posts Card' />
+          <div>
+            <p>Medium Posts Cards</p>
+            <p>Lorem ipsum dolor sit amet consectetur. Odio tortor pulvinar arcu duis in quam amet rhoncus. Sit sed gravida elit libero lectus suspendisse sit non ipsum. Tincidunt tortor molestie cursus gravida. Praesent in lectus quam ultricies magna erat.</p>
+            <Link href='https://github.com/david-fernando/medium-posts-card' target='_blank' >Acessar <IconExternalLink /></Link>
+          </div>
+        </span>
+
+        <span className={style.project}>
+          <Image src={mediumpostsapi} alt='Medium Posts API' />
+          <div>
+            <p>Medium Posts API</p>
+            <p>Lorem ipsum dolor sit amet consectetur. Odio tortor pulvinar arcu duis in quam amet rhoncus. Sit sed gravida elit libero lectus suspendisse sit non ipsum. Tincidunt tortor molestie cursus gravida. Praesent in lectus quam ultricies magna erat.</p>
+            <Link href='https://github.com/david-fernando/medium-posts-api' target='_blank' >Acessar <IconExternalLink /></Link>
+          </div>
+        </span>
+
       </div>
     </section>
   )
